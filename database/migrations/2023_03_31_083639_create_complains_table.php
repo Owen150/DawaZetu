fdf2414f46a84a19684c586b3b0818e35640e8b0
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-03-31 12:04:44 +0300

    Initiated Complains CRUD

---
+++ database/migrations/2023_03_31_083639_create_complains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complains', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('facility_id');
+            $table->string('status');
+            $table->string('type');
+            $table->string('note');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('facility_id')->references('id')->on('facilities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complains');
+    }
+};
