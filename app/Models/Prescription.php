60ac64aa3e6c7726c2a47f43b47c4ab9428b4ae4
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-04-14 23:45:04 +0300

    Prescription/Prescription Details Models

---
+++ app/Models/Prescription.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prescription extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'patient_number',
+        'patient_name',
+        'patient_address',
+        'doctor',
+        'prescription_date',
+        'prescription_cost'
+    ];
+}
